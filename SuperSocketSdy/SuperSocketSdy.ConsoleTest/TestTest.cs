7dbf71433e5921a9ab765f0ce37ebece903bc9a4
yzz <1277745281@qq.com>
2018-02-13 16:40:18 +0800

    -

--- SuperSocketSdy/SuperSocketSdy.ConsoleTest/TestTest.cs
+++ SuperSocketSdy/SuperSocketSdy.ConsoleTest/TestTest.cs
@@ -14,8 +14,21 @@ namespace SuperSocketSdy.ConsoleTest
 			Console.WriteLine( testServer.Setup(2012));
 			Console.WriteLine( testServer.Start());
 			testServer.NewRequestReceived += new QUERY().ExecuteCommand;
+			testServer.NewSessionConnected += TestServer_NewSessionConnected;
+			testServer.SessionClosed += TestServer_SessionClosed;
 			Console.WriteLine("The server started successfully, press key 'q' to stop it!");
 			Console.ReadKey();
+		}
+
+		private void TestServer_SessionClosed(TestSession session, SuperSocket.SocketBase.CloseReason reason)
+		{
+			Console.WriteLine("A session is closed for {0}.", reason);
+		}
+
+		private void TestServer_NewSessionConnected(TestSession session)
+		{
+			Console.WriteLine("有新连接");
+			session.Send("Welcome to SuperSocket Telnet Server");
 		}
 	}
 }
